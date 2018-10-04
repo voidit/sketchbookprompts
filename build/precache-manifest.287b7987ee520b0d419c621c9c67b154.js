454c81a961871e15e5dd5401fb25078aa6973396
Irena Romendik <kidnemor@gmail.com>
2018-10-04 19:36:27 -0400

    tracking build folder

---
+++ build/precache-manifest.287b7987ee520b0d419c621c9c67b154.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d928a209200b85e6bc9112bba1d6da1e",
+    "url": "/static/media/rainbow.d928a209.svg"
+  },
+  {
+    "revision": "4f029abfca0c9a2c1b847e886d3198a5",
+    "url": "/static/media/logo.4f029abf.svg"
+  },
+  {
+    "revision": "48ad6da204b4f474e1ad5952cb39fccf",
+    "url": "/static/media/aquarelle.48ad6da2.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3a93bf86e77eeb0da84",
+    "url": "/static/js/main.c3a93bf8.chunk.js"
+  },
+  {
+    "revision": "2cbb9cfb91afe81a8fca",
+    "url": "/static/js/1.2cbb9cfb.chunk.js"
+  },
+  {
+    "revision": "c3a93bf86e77eeb0da84",
+    "url": "/static/css/main.8383afa3.chunk.css"
+  },
+  {
+    "revision": "0a307f237c58938067d3400c951f4ae9",
+    "url": "/index.html"
+  }
+];
