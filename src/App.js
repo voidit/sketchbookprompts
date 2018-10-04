2b11d34e8bc2a3ba273db4b64e5176e375323ac2
Irena Romendik <kidnemor@gmail.com>
2018-10-04 18:51:56 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import rainbow from './assets/rainbow.svg'
+import aquarelle from './assets/aquarelle.svg'
+import logo from './assets/logo.svg'
+import promptsJSON from './prompts.json'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class Button extends Component {
+    render() {
+        return (
+            <a href={this.props.href}>
+                <img src={this.props.icon} alt={this.props.href} />
+            </a>
+        );
+    }
 }
 
-export default App;
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            prompts: [{
+                "draw":"3 unlikely objects together"
+            }],
+            currentPrompt: 'Draw...'
+        }
+        this.randomize = this.randomize.bind(this)
+    }
+
+    randomize() {
+        this.setState((state) => {
+            let whichOne = Math.floor(Math.random() * (state.prompts.length - 1))
+            return {
+                ...state,
+                currentPrompt: state.prompts[whichOne].draw
+            }
+        })
+    }
+
+    componentWillMount(state) {
+        this.setState({
+            ...state,
+            prompts: promptsJSON
+        })
+        this.randomize()
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={rainbow} className="App-logo" alt="logo"/>
+                    <h1 onClick={this.randomize}>Draw... {this.state.currentPrompt}</h1>
+                    <section className="links">
+                        <Button icon={aquarelle} href="http://aquarelle.us" />
+                        <Button icon={logo} href="http://voidit.net" />
+                    </section>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default App
